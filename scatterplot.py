6e1aa1bea4fea3eb4cdb1f433cf4821af0d3c016
SoumenChem <39962021+SoumenChem@users.noreply.github.com>
2020-09-24 15:34:55 +0200

    Update scatterplot.py

--- scatterplot.py
+++ scatterplot.py
@@ -3,7 +3,7 @@ import numpy as np
 import seaborn as sns
 from matplotlib import colors
 A = np.loadtxt('fftAll.dat')
-x,y=A.T
-plt.scatter(x, -y)
+x,y1,y2,y3=A.T
+plt.scatter(x, -(y1+y2+y3))
 plt.xlim(0,6)
 plt.savefig("fullspectra.png",dpi=300)
